b6737abe68984244ff5a3732addb3de321558ecc
Lee <171631592@qq.com>
2016-11-02 13:13:43 +0800

    add db

---
+++ app/src/main/java/com/coolweather/app/coolweather/model/Province.java
@@ -0,0 +1,34 @@
+package com.coolweather.app.coolweather.model;
+
+/**
+ * Created by DIY on 2016/11/2.
+ */
+public class Province {
+    private int id;
+    private String provinceName;
+    private String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
