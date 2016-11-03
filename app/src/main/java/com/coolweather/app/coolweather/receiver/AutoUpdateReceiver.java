79b2f472a89f2faed32c5858e2a46851c9aa17b5
Lee <171631592@qq.com>
2016-11-03 17:13:26 +0800

    增加后台自动更新天气的功能“ git commit -m 加后台自动更新天气的功能

---
+++ app/src/main/java/com/coolweather/app/coolweather/receiver/AutoUpdateReceiver.java
@@ -0,0 +1,18 @@
+package com.coolweather.app.coolweather.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.coolweather.app.coolweather.service.AutoUpdateService;
+
+/**
+ * Created by DIY on 2016/11/3.
+ */
+public class AutoUpdateReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Intent i=new Intent(context, AutoUpdateService.class);
+        context.startService(i);
+    }
+}
